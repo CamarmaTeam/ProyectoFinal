e226557f4c1aaa60d0b00b448cfc43c82bf9cd9f
NEDY-PC\Nedy <nnk_25_93@msn.com>
2018-11-15 18:20:41 +0100

    Rutas Log/SignIn

--- ClasesParticulares/src/app/app.module.ts
+++ ClasesParticulares/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent, 
-    FooterComponent
+    FooterComponent, LogInComponent, SignInComponent
   ],
   imports: [
     BrowserModule,
