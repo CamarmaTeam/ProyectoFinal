276cc1eac9721a425b3420e28825ff7dc3c56b9f
Nedyalko25 <nnk_25_93@msn.com>
2018-12-10 16:16:45 +0100

    nueva clase maquetada

--- ClasesParticulares/src/app/nueva-clase/nueva-clase.component.ts
+++ ClasesParticulares/src/app/nueva-clase/nueva-clase.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { ApiService } from '../api.service';
+
 
 @Component({
-  selector: 'app-nueva-clase',
-  templateUrl: './nueva-clase.component.html',
-  styleUrls: ['./nueva-clase.component.scss']
+	selector: 'app-nueva-clase',
+	templateUrl: './nueva-clase.component.html',
+	styleUrls: ['./nueva-clase.component.scss']
 })
 export class NuevaClaseComponent implements OnInit {
 
-  constructor() { }
+	mostrar: boolean;
 
-  ngOnInit() {
-  }
+	formClase: FormGroup;
+	constructor(private apiService: ApiService) {
+		this.formClase = new FormGroup({
+			nombre: new FormControl(''),
+			rama: new FormControl(''),
+			descripcion: new FormControl(''),
+			nivel: new FormControl(''),
+			claseIn: new FormControl(''),
+			claseOut: new FormControl(''),
+			enciudad: new FormControl(''),
+			fueraciudad: new FormControl('')
+		})
+		this.mostrar = false
+	}
+
+	ngOnInit() {
+	}
+
+	nuevaClase(){
+		
+	}
+	mostrarDistancia(){
+		this.mostrar = !this.mostrar
+	}
 
 }
